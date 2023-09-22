cdc79f4bb382e1203dd8388280aadefb45062d9d
swapnil chavan <chavanswapnil822@gmail.com>
2023-09-22 13:25:53 +0530

    updated the emojis

--- advertiser/src/Component/Iddata.jsx
+++ advertiser/src/Component/Iddata.jsx
@@ -505,7 +505,7 @@ navigate(`/account`, { state: iddata });
   return (
     <div style={{ padding: "10px" }}>
         <h1>Hello 👋 {lname} To Fill The Information</h1>
-        <h3 className="fill" onClick={handlenextdata}>👉 Click here to fill information 👈</h3>
+        <h3 className="fill" onClick={handlenextdata}>🌟👉 Click here to fill information 👈🌟</h3>
       <h1>Filled Information</h1>
       <button onClick={submit}>Submit</button>
       <br />
